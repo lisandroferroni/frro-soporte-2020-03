cf0295e9ddc8b45b4196863604ce92bcd1bc4086
tinchoabc425 <tincho.ballestero@gmail.com>
2020-07-29 18:44:55 -0300

    Búsqueda calle e intersección Parada de colectivo.

--- practico_08/tpi/schema.py
+++ practico_08/tpi/schema.py
@@ -3,21 +3,49 @@ from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 from models import *
 
 class Linea(SQLAlchemyObjectType):
+    id = graphene.ID(source='id', required=True)
     class Meta:
         model = LineaModel
         interfaces = (graphene.relay.Node,)
 
 class Parada(SQLAlchemyObjectType):
+    id = graphene.ID(source='id', required=True)
     class Meta:
         model = ParadaModel
         interfaces = (graphene.relay.Node, )
 
+class Calle(SQLAlchemyObjectType):
+    id = graphene.ID(source='id', required=True)
+    class Meta:
+        model = CalleModel
+        interfaces = (graphene.relay.Node, )
+
+class Interseccion(SQLAlchemyObjectType):
+    class Meta:
+        model = InterseccionModel
+        interfaces = (graphene.relay.Node, )
+
 class Query(graphene.ObjectType):
+    calles = graphene.List(Calle)
+    calles = graphene.List(Calle, q=graphene.String())
+    calleSearch = graphene.List(Calle, q=graphene.String())
+    interSearch = graphene.List(Interseccion, q=graphene.String())
     lineas = graphene.List(Linea)
     paradas = graphene.List(Parada)
+    all_calles = SQLAlchemyConnectionField(Calle)
     all_lineas = SQLAlchemyConnectionField(Linea)
     all_paradas = SQLAlchemyConnectionField(Parada)
     linea = graphene.Field(Linea, id = graphene.Int())
+    def resolve_calleSearch(self, info, **args):
+        q = args.get("q")  # Search query
+        query = Calle.get_query(info)  # SQLAlchemy query
+        calles = query.filter(CalleModel.nombre.contains(q)).all()
+        return calles
+    def resolve_interSearch(self, info, **args):
+        q = args.get("q")  # Search query
+        query = Interseccion.get_query(info)  # SQLAlchemy query
+        calles = query.filter(InterseccionModel.id_calle_1==q).join(CalleModel, CalleModel.id == InterseccionModel.id_calle_2, isouter=True).all()
+        return calles
     def resolve_lineas(self, info):
         query = Linea.get_query(info)  # SQLAlchemy query
         return query.all()
@@ -25,8 +53,6 @@ class Query(graphene.ObjectType):
         query = Linea.get_query(info)
         id = args.get('id')
         return query.get(id)
-
-
 
 schema = graphene.Schema(query=Query, types=[Linea])
 
